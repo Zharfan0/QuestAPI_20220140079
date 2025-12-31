fd6f7270411eda1166edbf717169e75d84eb0319
Zharfan <zharfanwidyadana@gmail.com>
2025-12-31 17:18:14 +0700

    membuat APP KEY untuk Aplikasi

--- app/src/main/java/com/example/questapi_20220140079/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questapi_20220140079/viewmodel/provider/PenyediaViewModel.kt
@@ -1,2 +1,9 @@
 package com.example.questapi_20220140079.viewmodel.provider
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.example.questapi_20220140079.repositori.AplikasiDataSiswa
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
+        )
