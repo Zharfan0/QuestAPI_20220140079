31536d66d2bfb55cb3d2977cb45af5e76abbc362
Zharfan <zharfanwidyadana@gmail.com>
2025-12-31 17:22:06 +0700

    deklarasi UiStateSiswa pada EditViewModel.kt

--- app/src/main/java/com/example/questapi_20220140079/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_20220140079/viewmodel/EditViewModel.kt
@@ -1,4 +1,11 @@
 package com.example.questapi_20220140079.viewmodel
 
-class EditViewModel {
-}
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+
+class EditViewModel(savedStateHandle: SavedStateHandle,private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
