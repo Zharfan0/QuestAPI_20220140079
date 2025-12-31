236f26a7c65642ee9320e3980efa2cea05b0efde
Zharfan <zharfanwidyadana@gmail.com>
2025-12-31 18:14:24 +0700

    commit untuk XAMPP

---
+++ baca1teman.php
@@ -0,0 +1,25 @@
+<?php
+include_once "koneks.php";
+
+$id = isset($_GET['id']) ? intval($_GET['id']) : 0;
+ 
+if ($id > 0) {
+$result = mysqli_query($conn,"SELECT * FROM teman WHERE id = '".$id."'");
+ 
+    if ($result->num_rows > 0) {
+$siswa = mysqli_fetch_assoc($result);
+header('Content-Type: application/json');
+echo json_encode($siswa);
+    } else {
+        header('HTTP/1.1 404 Not Found');
+        echo json_encode(['error' => 'Data tidak ditemukan']);
+        }
+}
+else
+{
+  header('HTTP/1.1 400 Bad Request');
+         echo json_encode(['error' => 'Invalid ID']);
+}
+ 
+mysqli_close($conn);
+?>
---
+++ bacateman.php
@@ -0,0 +1,14 @@
+(untuk membaca semua data yang berada pada tabel teman)
+ 
+<?php
+include_once "koneks.php";
+ 
+$result = mysqli_query($conn,"SELECT * FROM teman order by nama");
+$json = array();
+
+while($row = mysqli_fetch_assoc($result)){
+$json[] = $row;
+}
+echo json_encode($json);
+mysqli_close($conn);
+?>
