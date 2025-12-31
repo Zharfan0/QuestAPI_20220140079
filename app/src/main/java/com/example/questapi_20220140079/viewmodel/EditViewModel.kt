ab272281b7845e0d844c2aea72dbbd41579d90fa
Zharfan <zharfanwidyadana@gmail.com>
2025-12-31 17:22:48 +0700

    membuat value idSiswa jika notnull maka menginisiasi get

--- app/src/main/java/com/example/questapi_20220140079/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_20220140079/viewmodel/EditViewModel.kt
@@ -5,7 +5,21 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_20220140079.modeldata.UIStateSiswa
+import com.example.questapi_20220140079.modeldata.toUiStateSiswa
+import com.example.questapi_20220140079.repositori.RepositoryDataSiswa
+import com.example.questapi_20220140079.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
 
 class EditViewModel(savedStateHandle: SavedStateHandle,private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
